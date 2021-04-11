89819b63f64b9c949fb3944afa44fa9c9890a974
Tiger Wang <tigerwang@outlook.com>
2021-04-10 23:23:20 -0400

    added configuration for KafkaAdmin and topic

---
+++ src/main/java/com/wangxiaohu/kafkademo/KafkaConfiguration.java
@@ -0,0 +1,26 @@
+package com.wangxiaohu.kafkademo;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.KafkaAdmin;
+
+@Configuration
+public class KafkaConfiguration {
+
+    @Bean
+    public KafkaAdmin kafkaAdmin() {
+        Map<String, Object> configMap = new HashMap<>();
+        configMap.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        return new KafkaAdmin(configMap);
+    }
+
+    @Bean
+    public NewTopic demoTopic() {
+        return new NewTopic("demo", 1, (short) 1);
+    }
+}
